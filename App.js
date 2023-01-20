0bf0fcca0d3839493c6d94be31635c815e3371d0
Sahil pillania <sahilpillania88@gmail.com>
2023-01-20 11:43:36 +0530

    state

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+// import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, StatusBar, Text, View } from "react-native";
+import IntorScreen from "./views/IntorScreen";
+import MainScreen from "./views/MainScreen";
 
 export default function App() {
+  const [isloaded, setIsloaded] = useState(false);
+
+  setTimeout(() => {
+    setIsloaded(true);
+  }, 2000);
+
   return (
     <View style={styles.container}>
-      <Text>App loaded </Text>
       <StatusBar style="auto" />
+      {isloaded ? <MainScreen /> : <IntorScreen />}
+      {/* <IntorScreen /> */}
     </View>
   );
 }
